e7da81442d738e0499dba9a22bf61442a0da9e06
Ayush Aggarwal <Aama098@gmail.com>
2018-09-21 23:48:32 +0530

    adding new code

---
+++ Basic_Programming/life_universe_everything.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+	int x;
+
+
+
+
+	while(x!=42){
+
+        cin>>x;
+        
+if (x==42){
+
+            break;
+        }
+
+        cout<<x<<endl;
+
+	}
+
+	
+	return 0;
+
+}
