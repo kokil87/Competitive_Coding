c39b3d584df3872404e13ee73758be911d7d6396
Ayush Aggarwal <Aama098@gmail.com>
2018-09-25 23:42:25 +0530

    empty LL

--- Data Strutures/LInked List/Operations.cpp
+++ Data Strutures/LInked List/Operations.cpp
@@ -6,3 +6,19 @@ struct node {
  int data;
  node *next;
 }
+
+class linked_list
+{
+ private: int *head, *tail;
+ public: 
+ linked_list()
+  {
+   head = null;
+   tail = null;
+  }
+ }
+int main()
+{
+linked_list a;
+ return 0;
+}
