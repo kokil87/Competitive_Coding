cd5eed1b135f3ddf573752bb0b56bf37033d6ab4
Ayush Aggarwal <Aama098@gmail.com>
2018-09-26 00:29:42 +0530

    Adding a node

--- Data Strutures/LInked List/Operations.cpp
+++ Data Strutures/LInked List/Operations.cpp
@@ -9,17 +9,35 @@ struct node {
 
 class linked_list
 {
- private: 
- node *head, *tail;
- public: 
- linked_list()
-  {
-   head = null;
-   tail = null;
-  }
- }
+     private: 
+     node *head, *tail;
+     public: 
+     linked_list()
+    {
+        head = null;
+        tail = null;
+     }
+       void add_node(int n)
+       {
+          node *tmp = new node;
+          tmp -> data = n;
+          tmp -> next = NULL;
+   
+          if(head == NULL)
+          {
+                head = tmp;
+                tail = null;
+           }
+          else {
+                tail -> next = tmp;
+                tail = tail -> next;
+                }
+         }
+ };
 int main()
 {
 linked_list a;
+ a.add_node(1);
+ a.add_node(2);
  return 0;
 }
